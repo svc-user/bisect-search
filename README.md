04de2e36c8ca436a28dc03b72424481727aab03f
Nikolaj Schomacker <nsc@acies.dk>
2024-03-05 10:03:26 +0100

    A useful state.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # bisect-search
 grep-like utility for use with `git bisect run` that sets the correct exit codes for git bisect to pick up.
+
+## What it solves
+
+From the [git bisect run](https://git-scm.com/docs/git-bisect#_bisect_run) documentation we can see that the `run` subcommand expects a return code indicating wether a commit is bad/new or good/old.
+
+For powershell on windows this is not straightforward to set the exit code correctly using scripts, so I made a binary, which works much better with git 🤷‍♂️
+
+## How to use
+
+`bisect-search` is simply a tool that opens a file, searches for a string and sets exit code 0/1/125/255 depending on whether there's a match, the file is missing or something went wrong.
+
+Running `bisect-search.exe` with no arguments prints the help
+```
+----------------------------------------------------------------
+Search a file for a string and set a return code for git bisect.
+----------------------------------------------------------------
+        --inverse, -i    Inverse the exit code so that a match indicates a bad/newer commit.
+                (default: false)
+        --verbose        Print debug output when executed.
+                (default: false)
+        --file, -f <string value>        Relative or absolute path of file to search in.
+        --string, -s <string value>      String to search for in the given file.
+```
+
+To use it with `git bisect` simply run `git bisect run .\bisect-search.exe -f .\my-changed-file.zig -s "string to search for"`.
+
+## Return codes
+ * If the search string is found return **0** (good/old commit). If `--inverse` is set returns **1** (bad/new commit).
+ * Is the file not found `bisect-search` returns **125** which tells git to skip the commit.
+ * If something goes wrong a return code **255** is set, aborting `git bisect run`.
---
+++ src/main.zig
@@ -0,0 +1,135 @@
+const std = @import("std");
+const ArgParser = @import("Argparser.zig");
+const Arg = ArgParser.Arg;
+
+const CODE_BISECT_ABORT = 255;
+const CODE_BISECT_SKIP = 125;
+const CODE_BISECT_GOOD = 0;
+const CODE_BISECT_BAD = 1;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var stderr = std.io.getStdErr().writer();
+    var stdout = std.io.getStdOut().writer();
+
+    const arg_verbose = "verbose";
+    const arg_inverse = "inverse";
+    const arg_file = "file";
+    const arg_string = "string";
+    const parser = comptime ArgParser.Parser("Search a file for a string and set a return code for git bisect.", &[_]Arg{
+        .{ .longName = arg_inverse, .shortName = 'i', .description = "Inverse the exit code so that a match indicates a bad/newer commit.", .argType = .bool, .default = "false", .isOptional = true },
+        .{ .longName = arg_verbose, .description = "Print debug output when executed.", .argType = .bool, .default = "false", .isOptional = true },
+        .{ .longName = arg_file, .shortName = 'f', .description = "Relative or absolute path of file to search in.", .argType = .string, .isOptional = false },
+        .{ .longName = arg_string, .shortName = 's', .description = "String to search for in the given file.", .argType = .string, .isOptional = false },
+    });
+
+    var command_line_args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, command_line_args);
+
+    var args = parser.parse(allocator, command_line_args) catch |err| {
+        switch (err) {
+            ArgParser.ParseError.InvalidArgumentValue => {
+                try stderr.print("ERROR: Invalid argument value\n", .{});
+            },
+            ArgParser.ParseError.MissingArgument => {
+                try stderr.print("ERROR: Missing argument.\n", .{});
+            },
+            else => {},
+        }
+        try parser.printHelp(std.io.getStdErr());
+        std.os.exit(CODE_BISECT_ABORT);
+    };
+    defer args.deinit();
+
+    const bin_name = std.fs.path.basename(command_line_args[0]);
+    const inverse = args.get(arg_inverse).?.value().bool;
+    const verbose = args.get(arg_verbose).?.value().bool;
+    const search_file_path = args.get(arg_file).?.value().string;
+    const search_string = args.get(arg_string).?.value().string;
+
+    if (verbose) {
+        try stdout.print("{s}: Inverse match? {s}\n", .{ bin_name, if (inverse) "yes" else "no" });
+        try stdout.print("{s}: Verbose output? {s}\n", .{ bin_name, if (verbose) "yes" else "no" });
+        try stdout.print("{s}: Searched file is \"{s}\"\n", .{ bin_name, search_file_path });
+        try stdout.print("{s}: Search string is \"{s}\"\n", .{ bin_name, search_string });
+    }
+
+    const abs_path = std.fs.cwd().realpathAlloc(allocator, search_file_path) catch {
+        try stderr.print("{s}: unable to open file: {s}\n", .{ bin_name, search_file_path });
+        std.os.exit(CODE_BISECT_SKIP);
+    };
+    defer allocator.free(abs_path);
+
+    if (verbose) {
+        try stdout.print("{s}: Opening file {s}\n", .{ bin_name, abs_path });
+    }
+
+    // std.debug.print("{s}\n", .{abs_path});
+    var file = std.fs.cwd().openFile(search_file_path, .{}) catch {
+        try stderr.print("{s}: unable to open file: {s}\n", .{ bin_name, abs_path });
+        std.os.exit(CODE_BISECT_SKIP);
+    };
+    defer file.close();
+
+    if (verbose) {
+        try stdout.print("{s}: File handle is 0x{x:0>4}\n", .{ bin_name, @intFromPtr(file.handle) });
+    }
+
+    // const file_cont = try file.readToEndAlloc(allocator, std.math.maxInt(u64));
+    // defer allocator.free(file_cont);
+
+    var BuffReader = std.io.bufferedReader(file.reader());
+    var fls = BuffReader.reader();
+
+    var al = std.ArrayList(u8).init(allocator);
+    defer al.deinit();
+
+    while (true) {
+        fls.streamUntilDelimiter(al.writer(), '\n', null) catch {
+            break;
+        };
+        const line = try std.mem.concat(allocator, u8, &[_][]const u8{ al.items, "\n" });
+        defer allocator.free(line);
+        // std.debug.print("{s}", .{line});
+
+        if (std.mem.indexOf(u8, line, search_string)) |idx| {
+            if (idx > -1) {
+                if (verbose) {
+                    try stdout.print("{s}: Found search string!\n", .{bin_name});
+                }
+
+                if (inverse) {
+                    if (verbose) {
+                        try stdout.print("{s}: Exit code is {d}\n", .{ bin_name, CODE_BISECT_BAD });
+                    }
+                    std.os.exit(CODE_BISECT_BAD);
+                } else {
+                    if (verbose) {
+                        try stdout.print("{s}: Exit code is {d}\n", .{ bin_name, CODE_BISECT_GOOD });
+                    }
+                    std.os.exit(CODE_BISECT_GOOD);
+                }
+            }
+        }
+
+        al.items.len = 0;
+    }
+
+    if (verbose) {
+        try stdout.print("{s}: Search string not found in file.\n", .{bin_name});
+    }
+    if (inverse) {
+        if (verbose) {
+            try stdout.print("{s}: Exit code is {d}\n", .{ bin_name, CODE_BISECT_GOOD });
+        }
+        std.os.exit(CODE_BISECT_GOOD);
+    } else {
+        if (verbose) {
+            try stdout.print("{s}: Exit code is {d}\n", .{ bin_name, CODE_BISECT_BAD });
+        }
+        std.os.exit(CODE_BISECT_BAD);
+    }
+}
